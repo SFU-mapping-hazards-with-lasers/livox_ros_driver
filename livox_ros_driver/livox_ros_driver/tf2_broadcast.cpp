f528dd7d3ffddd5f9720263fa96caade7a9d9043
jay <jay.matsushiba@gmail.com>
2023-09-27 11:46:45 -0700

    updated tf_broadcast to be able to rename parent frame

--- livox_ros_driver/livox_ros_driver/tf2_broadcast.cpp
+++ livox_ros_driver/livox_ros_driver/tf2_broadcast.cpp
@@ -9,13 +9,14 @@
 #include <tf2/LinearMath/Quaternion.h>
 
 std::string static_livox_name;
+std::string static_parent_name;
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "my_static_tf2_broadcaster");
-    if (argc != 8)
+    if (argc != 9)
     {
-        ROS_ERROR("Invalid number of parameters\nusage: static_tf2_broadcaster child_frame_name x y z roll pitch yaw");
+        ROS_ERROR("Invalid number of parameters\nusage: static_tf2_broadcaster child_frame_name parent_frame_name x y z roll pitch yaw");
         return -1;
     }
     if (strcmp(argv[1], "world") == 0)
@@ -24,20 +25,21 @@ int main(int argc, char **argv)
         return -1;
     }
     static_livox_name = argv[1];
+    static_parent_name = argv[2];
     static tf2_ros::StaticTransformBroadcaster static_broadcaster;
     // Create a TransformStamped message
     geometry_msgs::TransformStamped transformStamped;
     transformStamped.header.stamp = ros::Time::now();    // Set the timestamp
-    transformStamped.header.frame_id = "world";          // Parent frame ID
+    transformStamped.header.frame_id = static_parent_name;          // Parent frame ID
     transformStamped.child_frame_id = static_livox_name; // Child frame ID
 
     // Set the transformation (position and orientation)
-    transformStamped.transform.translation.x = atof(argv[2]);
-    transformStamped.transform.translation.y = atof(argv[3]);
-    transformStamped.transform.translation.z = atof(argv[4]);
+    transformStamped.transform.translation.x = atof(argv[3]);
+    transformStamped.transform.translation.y = atof(argv[4]);
+    transformStamped.transform.translation.z = atof(argv[5]);
 
     tf2::Quaternion quat;
-    quat.setRPY(atof(argv[5]), atof(argv[6]), atof(argv[7]));
+    quat.setRPY(atof(argv[6]), atof(argv[7]), atof(argv[8]));
 
     transformStamped.transform.rotation.x = quat.x();
     transformStamped.transform.rotation.y = quat.y();
